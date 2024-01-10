6c02596b4c5505d01aee6998deb6e0af1e057766
Ubuntu <ubuntu@ip-172-31-41-244.us-east-2.compute.internal>
2024-01-10 09:40:53 +0000

    shellcheck adding diff

--- shellcheck-prec-commit.sh
+++ shellcheck-prec-commit.sh
@@ -8,16 +8,23 @@ SHELLCHECK_V="shellcheck-v0.9.0"
 BINARY_SHELLCHECK="/tmp/${SHELLCHECK_V}/shellcheck"
 
   if [[ ! -f ${BINARY_SHELLCHECK} ]]; then
-  wget -q https://github.com/koalaman/shellcheck/releases/download/v0.9.0/${SHELLCHECK_V}.linux.x86_64.tar.xz -O /tmp/${SHELLCHECK_V}.tar.xz
+    wget -q https://github.com/koalaman/shellcheck/releases/download/v0.9.0/${SHELLCHECK_V}.linux.x86_64.tar.xz -O /tmp/${SHELLCHECK_V}.tar.xz
 
 
-  tar -xvf /tmp/${SHELLCHECK_V}.tar.xz -C /tmp > /dev/null
-  rm -f /tmp/${SHELLCHECK_V}.tar.xz
+    tar -xvf /tmp/${SHELLCHECK_V}.tar.xz -C /tmp > /dev/null
+    rm -f /tmp/${SHELLCHECK_V}.tar.xz
   fi
 else 
- SHELL_B=$(command -v shellcheck )
+  SHELL_B="$(command -v shellcheck)"
 fi
     # run against all .sh files or shellscripts ending with .sh
 SHELL_FILES=$(git diff --cached --name-only | grep -E '.sh$')
 
-${BINARY_SHELLCHECK} ${SHELL_FILES} --severity=warning
+if [[ -n ${SHELL_FILES} ]] ; then  
+ ${BINARY_SHELLCHECK} ${SHELL_FILES} --severity=warning
+ 
+  if [[ $? -ne 0  ]]; then 
+   echo -e "\nERROC CHECK : check script for ERROR or Warnings" 
+   exit 1
+  fi
+fi  
