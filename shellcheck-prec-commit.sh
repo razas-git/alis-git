e651a65a0d80ee2755ee61bf61993773e0b38df2
Ubuntu <ubuntu@ip-172-31-41-244.us-east-2.compute.internal>
2024-01-10 08:28:24 +0000

    new verison of shellcommis.sh

--- shellcheck-prec-commit.sh
+++ shellcheck-prec-commit.sh
@@ -3,20 +3,21 @@
 
 
 #TODO: Install shellcheck utility
-if ! command -v shellcheck > /dev/null ; then
+if ! command -v shellcheck > /dev/null; then
 SHELLCHECK_V="shellcheck-v0.9.0"
 BINARY_SHELLCHECK="/tmp/${SHELLCHECK_V}/shellcheck"
 
-if [[! -f ${BINARY_SHELLCHECK} ]]
-wget -q https://github.com/koalaman/shellcheck/releases/download/v0.9.0/${SHELLCHECK_V}.linux.x86_64.tar.xz -O /tmp/${SHELLCHECK_V}.tar.xz
+  if [[ ! -f ${BINARY_SHELLCHECK} ]]; then
+  wget -q https://github.com/koalaman/shellcheck/releases/download/v0.9.0/${SHELLCHECK_V}.linux.x86_64.tar.xz -O /tmp/${SHELLCHECK_V}.tar.xz
 
 
-tar -xvf /tmp/${SHELLCHECK_V}.tar.xz -C /tmp > /dev/null
-rm -f /tmp/${SHELLCHECK_V}.tar.xz
-fi 
- else 
- SHELL_B=$( command -v shellcheck )
+  tar -xvf /tmp/${SHELLCHECK_V}.tar.xz -C /tmp > /dev/null
+  rm -f /tmp/${SHELLCHECK_V}.tar.xz
+  fi
+else 
+ SHELL_B=$(command -v shellcheck )
+fi
+    # run against all .sh files or shellscripts ending with .sh
+SHELL_FILES=$(git diff --cached --name-only | grep -E '.sh$')
 
-
-#TODO: run against all .sh files or shellscripts
-${BINARY_SHELLCHECK} csp-authentication.sh --severity=warning
+${BINARY_SHELLCHECK} ${SHELL_FILES} --severity=warning
